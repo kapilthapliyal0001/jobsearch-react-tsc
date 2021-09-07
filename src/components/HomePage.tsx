e13328ca34165f689ad6152b62654e1e9acd1f87
kapilthapliyal0001 <kapilthapliyal0001@gmail.com>
2021-09-07 11:49:26 +0200

    basic setup

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+export default class HomePage extends Component {
+  static propTypes = {
+    data: [],
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
